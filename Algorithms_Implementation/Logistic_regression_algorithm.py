7e5816087fdb0735127b48ca09b72bdc61e00005
hazem mohamed <hazemm7622@gmail.com>
2023-12-14 19:50:07 +0200

    solve error in dataset

--- Algorithms_Implementation/Logistic_regression_algorithm.py
+++ Algorithms_Implementation/Logistic_regression_algorithm.py
@@ -1,17 +1,23 @@
-# Import necessary libraries
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+
 # Load the dataset
-data = pd.read_csv('email_spam.csv')
+data = pd.read_csv('email_spam.csv').drop("Unnamed: 0.1",axis=1).drop("Unnamed: 0",axis=1)
+data.head()
+
+print(data.isna().sum())
+
 
 # Split the dataset into training and testing data
-X = data.drop("y")
-y = data['type']
+X = data["Body"].values
+y = data['Label'].values
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 
 # Convert email text into numerical features using CountVectorizer
 vectorizer = CountVectorizer()
@@ -22,17 +28,18 @@ X_test_vec = vectorizer.transform(X_test)
 model = LogisticRegression()
 model.fit(X_train_vec, y_train)
 
+
 # Make predictions on the test data
 y_pred = model.predict(X_test_vec)
 
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, pos_label='spam')
-recall = recall_score(y_test, y_pred, pos_label='spam')
-f1 = f1_score(y_test, y_pred, pos_label='spam')
+precision = precision_score(y_test, y_pred, pos_label=1)
+recall = recall_score(y_test, y_pred, pos_label=1)
+f1 = f1_score(y_test, y_pred, pos_label=1)
 
 # Print the model performance metrics
-print("Accuracy:", accuracy)
-print("Precision:", precision)
-print("Recall:", recall)
-print("F1 Score:", f1)
+print("Accuracy:", accuracy*100)
+print("Precision:", precision*100)
+print("Recall:", recall*100)
+print("F1 Score:", f1*100)
